de1e0e0b7ffe981d72db46cdf75f77b4d852c329
Ishors <antoine.cap@hotmail.fr>
2021-04-14 22:09:55 +0200

    :pencil2: changing all "dash" names and variables to mine on the "left" page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('FulfilledCtrl', function($scope) {})
 
 .controller('ToFulfillCtrl', function($scope, BucketList) {
   // With the new view caching in Ionic, Controllers are only called
